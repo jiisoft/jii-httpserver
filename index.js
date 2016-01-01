c9ec5a055030a535be419afb13dd05f00b9fe6ff
Kozhin Vladimir <affka@affka.ru>
2016-01-01 21:47:36 +0700

    auto-build comment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = require('jii');
 
+// AUTO-GENERATED Package files list
 require('./lib/server/HttpServer.js');
 require('./lib/server/Request.js');
 require('./lib/server/Response.js');
