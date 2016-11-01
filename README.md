be8e50267b650586088b1a1de91c0a57d0016d0d
Kozhin Vladimir <affka@affka.ru>
2016-11-01 22:17:04 +0700

    deprecate package

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-jii-httpserver [![Build Status](https://travis-ci.org/jiisoft/jii-httpserver.svg)](https://travis-ci.org/jiisoft/jii-httpserver)
-======================
-
-### Install
-
-```sh
-npm install jii jii-httpserver
-```
+# jii-httpserver
+Deprecated. Package moved to jii.
--- index.js
+++ index.js
@@ -1,8 +1,2 @@
+console.warn('Package jii-httpserver is deprecated and moved to jii package.');
 module.exports = require('jii');
-
-// AUTO-GENERATED Package files list
-if (!process.env.JII_NO_NAMESPACE) {
-    require('./server/HttpServer.js');
-    require('./server/Request.js');
-    require('./server/Response.js');
-}
